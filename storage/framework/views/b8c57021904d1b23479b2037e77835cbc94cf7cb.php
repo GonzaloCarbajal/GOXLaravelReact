2cd81883b21ad96819c1d429ce6d3cdb9e2d4d3a
CARBAJAL GONZALO <gonzacarbajal18@gmail.com>
2021-03-31 13:02:20 -0300

    Segundo Commit

---
+++ storage/framework/views/b8c57021904d1b23479b2037e77835cbc94cf7cb.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Styles -->
+    <link href="<?php echo e(asset('css/app.css')); ?>" rel="stylesheet">
+</head>
+<body class="antialiased">
+<div id="home">
+
+</div>
+
+<!-- React JS -->
+<script src="<?php echo e(asset('js/app.js')); ?>" defer></script>
+</body>
+</html>
+<?php /**PATH C:\xampp\htdocs\GOXLaravelReact\resources\views///HomeIndex/HomeIndex.blade.php ENDPATH**/ ?>
